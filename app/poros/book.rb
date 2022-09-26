02125486cee0d0b17e166edd93312aee48daab64
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-26 10:16:22 -0600

    create book serializer

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -1,10 +1,11 @@
 class Book
-    attr_reader :title, :isbn, :publisher
+    attr_reader :title, :isbn, :publisher, :id
 
     def initialize(data)
         @isbn = data[:edition_key]
         @title = data[:title]
         @publisher = data[:contributor]
+        @id = nil
     end
     
 end
