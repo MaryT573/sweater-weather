e59a0e2a3b0218c9e666b11522033892eb83b0a3
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-27 22:59:20 -0600

    added sad path to users, created happy and sad path for sessions

--- spec/requests/api/v1/session_request_spec.rb
+++ spec/requests/api/v1/session_request_spec.rb
@@ -3,21 +3,48 @@ require 'rails_helper'
 RSpec.describe 'Sessions Endpoint' do
   describe 'Login' do
     it 'checks for exisiting user' do
-      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
-      user_params = {
-          "username": "whatever@example.com",
-          "password": "password"
-      }
+        user_params = {
+            "username": "whatever@example.com",
+            "password": "password",
+            "password_confirmation": "password"
+        }
+        post '/api/v1/users', params: user_params
 
-      post '/api/v1/sessions', headers: headers, params: JSON.generate(user_params)
-      result = JSON.parse(response.body, symbolize_names: true)
+        user_params = {
+            "username": "whatever@example.com",
+            "password": "password",
+        }
+        post '/api/v1/sessions', params: user_params
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+
+        result = JSON.parse(response.body, symbolize_names: true)
       
-      expect(result).to be_a Hash
-      expect(result[:data]).to have_key(:id)
-      expect(result[:data][:type]).to eq("users")
+        expect(result).to be_a Hash
+        expect(result[:data]).to have_key(:id)
+        expect(result[:data][:type]).to eq("users")
 
-      expect(result[:data][:attributes][:username]).to eq(user_params[:username])
-      expect(result[:data][:attributes][:api_key]).to be_a String
+        expect(result[:data][:attributes][:username]).to eq(user_params[:username])
+        expect(result[:data][:attributes][:api_key]).to be_a String
+    end
+
+    it 'fails if password doesnt match' do
+        user_params = {
+            "username": "whatever@example.com",
+            "password": "password",
+            "password_confirmation": "password"
+        }
+        post '/api/v1/users', params: user_params
+
+        user_params = {
+            "username": "whatever@example.com",
+            "password": "wrong",
+        }
+        post '/api/v1/sessions', params: user_params
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
     end
   end
 end
