691c7e1a6c550b9da37f8df54d4a577fa3516da7
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-28 14:59:15 -0600

    roadtrip facade

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MapFacade do
-    xit 'reads data from api' do
+    it 'reads data from api' do
         latlong = MapFacade.find_lat_long("denver, co")          
         
         expect(latlong).to be_a Hash
@@ -9,5 +9,11 @@ RSpec.describe MapFacade do
     end
 
     it "reads direction data and feeds into poro" do
+        map = MapFacade.route( "denver, co", "Estes Park, CO" )
+         
+        expect(map).to be_a RoadTrip
+        expect(map.blocked).to eq(false)
+        expect(map.bordercrossing).to eq(false)
+        expect(map.time).to eq("01:22:19")
     end
 end
