0df831daf0dae5059fc9ace7d07806875a642fd0
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-28 19:50:04 -0600

    vcr fixtures

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Forecast do
-    it 'has attributes of daily weather' do
+    it 'has attributes of daily weather', :vcr do
         data = WeatherFacade.forecast({:lat=>39.738453, :lng=>-104.984853})
  
         expect(data).to be_a Forecast
